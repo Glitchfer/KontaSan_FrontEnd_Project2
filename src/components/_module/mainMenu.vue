a4e91cd4079681533cb9560870315b1d3d08aaec
Glitchfer <arif.0496.rahman@gmail.com>
2020-09-14 19:56:12 +0700

    update env

--- src/components/_module/mainMenu.vue
+++ src/components/_module/mainMenu.vue
@@ -29,7 +29,7 @@
       >
         <div
           class="img1 a1"
-          v-bind:style="{ backgroundImage: `url(${urlApi}${item.img})` }"
+          v-bind:style="{ backgroundImage: `url(${urlApi2}${item.img})` }"
           v-on:click="selectMenu(index, item)"
         >
           <div v-if="count < 1" class="selected">
@@ -60,6 +60,7 @@ export default {
   data() {
     return {
       urlApi: process.env.VUE_APP_URL,
+      urlApi2: process.env.VUE_APP_URL,
       invoice_id: '',
       invoiceData: [],
       invoiceMsg: [],
